3ba6945870f79c2d91758e5fa72905196b54cb97
prashanth-g <prashanthcsgce@gmail.com>
2019-03-01 17:35:11 +0530

    Added demo data

--- src/main/java/com/prashanth/spring/reactive/SpringReactiveApplication.java
+++ src/main/java/com/prashanth/spring/reactive/SpringReactiveApplication.java
@@ -1,10 +1,25 @@
 package com.prashanth.spring.reactive;
 
+import com.prashanth.spring.reactive.model.Movie;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SpringReactiveApplication {
+
+    @Bean
+    ApplicationRunner sampleData(MovieRepository movieRepository) {
+        return args -> {
+            Flux.just("Spring is coming soon", "Spring: The future", "Spring is cool")
+                    .map(Movie::new)
+                    .flatMap(movieRepository::save)
+                    .subscribe(System.out::println);
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringReactiveApplication.class, args);
     }
