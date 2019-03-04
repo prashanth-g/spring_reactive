351838ac45f521ef28f381ded78dc820520617a1
prashanth-g <prashanthcsgce@gmail.com>
2019-03-04 22:18:34 +0530

    Added save and get methods in MovieService

--- src/main/java/com/prashanth/spring/reactive/service/MovieService.java
+++ src/main/java/com/prashanth/spring/reactive/service/MovieService.java
@@ -1,4 +1,32 @@
 package com.prashanth.spring.reactive.service;
 
+import com.prashanth.spring.reactive.MovieRepository;
+import com.prashanth.spring.reactive.model.Movie;
+import com.prashanth.spring.reactive.model.MovieEvent;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.Date;
+
 public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public Flux<Movie> getAllMovies() {
+        return this.movieRepository.findAll();
+    }
+
+    public Mono<Movie> getMovieById(String movieId) {
+        return this.movieRepository.findById(movieId);
+    }
+
+    public Flux<MovieEvent> getEvents(String movieId) {
+        return Flux.<MovieEvent>generate(sink -> sink.next(new MovieEvent(movieId, new Date())))
+                .delayElements(Duration.ofSeconds(1));
+    }
 }
